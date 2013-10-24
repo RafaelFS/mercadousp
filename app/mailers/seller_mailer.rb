56a1658c8a178801305ab9a1a27d3129a0311b73
RafaelFS <rfl.fsilva@gmail.com>
2013-10-24 01:22:11 -0200

    Added manual configuration to email

--- app/mailers/seller_mailer.rb
+++ app/mailers/seller_mailer.rb
@@ -1,7 +1,7 @@
 class SellerMailer < ActionMailer::Base
   default from: "mercadousp@praticas.com"
-  def sold_email(sale)
-    @sale = sale
+  def sold_email(product)
+    @sale = product
     @url  = 'http://example.com/login'
     mail(to: @sale.email, subject: 'Welcome to My Awesome Site')
   end
